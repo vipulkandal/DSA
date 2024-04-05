c4fe761da5466ed5c736ca129dad0c03469ed5de
vipulkandal <vipulkandal@gmail.com>
2024-04-05 19:27:16 +0530

    Rotate array

---
+++ first.js
@@ -0,0 +1,11 @@
+const bruteForce = (nums, k) => {
+  return "bruteForce";
+};
+
+const main = (nums, k) => {
+  return bruteForce(nums, k);
+};
+
+const nums = [1, 2, 3, 4, 5, 6, 7];
+k = 3;
+console.log(main(nums, k));
