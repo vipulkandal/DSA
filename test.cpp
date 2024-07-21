07b0d0d38e9a5f3caf506dfaff421d49d3fde7e9
vipulkandal <vipu.kandal@gmail.com>
2024-07-21 19:59:34 +0530

    Spiral on INdex

--- test.cpp
+++ test.cpp
@@ -5,24 +5,49 @@ using namespace std;
 
 int main()
 {
-    int arr1[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int arr2[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int arr3[3][3];
-    // i: row, j: column
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            arr3[i][j] = arr1[i][j] + arr2[i][j];
-        }
-    }
+    const int row = 3;
+    const int col = 4;
+    int arr[row][col] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
 
-    // Printing the array
-    for (int i = 0; i < 3; i++)
+    int top = 0;
+    int bottom = row - 1;
+    int left = 0;
+    int right = col - 1;
+
+    while (top <= bottom && left <= right)
     {
-        for (int j = 0; j < 3; j++)
+        // Left to Right
+        for (int i = left; i <= right; i++)
         {
-            cout << arr3[i][j] << " ";
+            cout << arr[top][i] << " ";
+        }
+        top++;
+
+        // Top to Bottom
+        for (int i = top; i <= bottom; i++)
+        {
+            cout << arr[i][right] << " ";
+        }
+        right--;
+
+        if (top <= bottom)
+        {
+            // Right to Left
+            for (int i = right; i >= left; i--)
+            {
+                cout << arr[bottom][i] << " ";
+            }
+            bottom--;
+        }
+
+        if (left <= right)
+        {
+            // Bottom to Top
+            for (int i = bottom; i >= top; i--)
+            {
+                cout << arr[i][left] << " ";
+            }
+            left++;
         }
     }
     return 0;
